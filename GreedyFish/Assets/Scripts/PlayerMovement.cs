0a07d802327a7508d426a1e9797144fe5addbc80
Ximena Camacho <xcamacho@usc.edu>
2026-03-02 00:37:46 -0800

    flip fish when we swim

--- GreedyFish/Assets/Scripts/PlayerMovement.cs
+++ GreedyFish/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,22 +8,51 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     public BuoyancyEffector2D ocean;
+    //0 is left, 1 is right;
+    public int direction = 0;
+    public int oldDirection = 0;
     
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-       
+
     }
 
     private void Update()
     {
-        if (movement.x < 0){
+        if (movement.x < 0)
+        {
+            oldDirection = direction;
+            direction = 0;
             ocean.flowMagnitude = -30;
+            
         }
-        else if(movement.x > 0){
+        else if (movement.x > 0)
+        {
+            oldDirection = direction;
+            direction = 1;
             ocean.flowMagnitude = 30;
+            
+        }
+
+        if (oldDirection != direction)
+        {
+            //flip image
+            if (oldDirection == 1)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                
+
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+
+
+            }
+            
         }
         
         movement.x = Input.GetAxisRaw("Horizontal");
